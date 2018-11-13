6ff0c3ad4a7775b2902807ffa231856e93f6e5a7
Luke Holman <luke.holman@unimelb.edu.au>
2018-11-13 12:42:55 +1100

    Add dir to hold results files on Spartan

--- run.R
+++ run.R
@@ -1,6 +1,8 @@
 # module load R/3.4.0-GCC-4.9.2
 # cd /data/projects/punim0243/W_shredder
 
+if(!dir.exists("data/sim_results")) dir.create("data/sim_results")
+
 knitr::purl("analysis/run_model.Rmd")
 source("run_model.R")
 unlink("run_model.R")
