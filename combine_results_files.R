40f47e18be22fd890ff74c79ccbd08139c64b9a1
Luke Holman <luke.holman@unimelb.edu.au>
2018-11-30 11:34:27 +1100

    Tweak combine script

--- combine_results_files.R
+++ combine_results_files.R
@@ -12,23 +12,22 @@ custom_functions <- ls()
 working_directory <- "/data/projects/punim0243/W_shredder"
 setwd(working_directory)
 
-cpus <- 4
-sopt <- list(time = '18:00:00',   # time in hours
-             mem  = '51200'     # 204800 - 200GB memory across all 8 cores
-             # partition = 'physical'
-             )      # for extra memory
+cpus <- 8
+sopt <- list(time = '4:00:00',   # time in hours
+             mem  = '32768')     # 32GB ram
 
 
-sjob <- slurm_apply(function(i) {
-  saveRDS(combine_results_files(cores = cpus,
-                                wd = working_directory),
-          file = "/data/projects/punim0243/W_shredder/data/all_results.rds")
-},
-data.frame(i = 1),
-add_objects = c("cpus",
-                "working_directory",
-                custom_functions),
-jobname = 'combine_files',
-nodes = 1,
-cpus_per_node = cpus,
-slurm_options = sopt)
+sjob <- slurm_apply(
+  f = function(i) {
+    saveRDS(combine_results_files(cores = cpus,
+                                  wd = working_directory),
+            file = "/data/projects/punim0243/W_shredder/data/all_results.rds")},
+  params = data.frame(i = 1),
+  add_objects = c("cpus",
+                  "working_directory",
+                  custom_functions),
+  jobname = 'combine_files',
+  nodes = 1,
+  cpus_per_node = cpus,
+  slurm_options = sopt
+)
