3b6cc7a7503aa3ff424bbda223aca38babc6f5b4
Luke Holman <luke.holman@unimelb.edu.au>
2019-02-05 12:49:34 +1100

    Making graphs, fixed combine file

--- combine_results_files.R
+++ combine_results_files.R
@@ -1,3 +1,5 @@
+# rsync -aPz lukeholman@spartan:/data/projects/punim0243/W_shredder/data/sim_results /Users/lholman/Rprojects/W_shredder/data/sim_results
+
 source_rmd <- function(file){
   options(knitr.duplicate.label = "allow")
   tempR <- tempfile(tmpdir = ".", fileext = ".R")
@@ -17,8 +19,11 @@ sopt <- list(time = '48:00:00',   # time in hours
 
 all_files <- list.files("data/sim_results", full.names = TRUE)
 print(paste("About to merge", length(all_files), "files"))
-all_files <- split(all_files, ceiling(seq_along(all_files) / 10^5))
+all_files <- split(all_files, ceiling(seq_along(all_files) / 10000))
 print(paste("Splitting them into", length(all_files), "chunks of up to 10,000"))
+
+plan("multicore")
+future_lapply(1:length(all_files), combine_results_files, all_files = all_files, wd = getwd())
 
 sjob <- slurm_apply(
   f = function(i) {
